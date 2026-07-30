d969a8f3b298d76157fe8a7b14119c5bc444735b
adou-a <18965042252@163.com>
2026-07-30 20:46:10 +0800

    connect FastAPI with ingredient service

--- src/api/main.py
+++ src/api/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from src.services.ingredient_repository import IngredientRepository
+from src.services.ingredient_service import IngredientService
 
 
 app = FastAPI()
+repository = IngredientRepository()
+service = IngredientService(repository)
+
+
 
 @app.get('/')
 def home():
@@ -9,3 +15,13 @@ def home():
          "name":"niacinamide"
      
     }
+
+@app.get('/ingredients/{name}')
+def get_ingredient(name:str):
+
+    ingredient = service.find_ingredient(name)
+
+    if ingredient is None:
+        return{'message':'ingredient not found'}
+
+    return ingredient
